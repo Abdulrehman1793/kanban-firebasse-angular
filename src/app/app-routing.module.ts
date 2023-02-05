0b5c788f659a85a39fab3aea0df0bfb69eeb920a
KHAN ABDULREHMAN <abdulrehman1793@gmail.com>
2023-02-05 20:18:00 +0530

    resolve authentication redirect

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,17 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import {
+  redirectLoggedInTo,
+  redirectUnauthorizedTo,
+  AuthGuard,
+} from '@angular/fire/auth-guard';
+
+const redirectUnauthorizedToLogin = () => redirectUnauthorizedTo(['auth']);
+const redirectLoggedInToItems = () => redirectLoggedInTo(['.']);
+
 const routes: Routes = [
   {
     path: '',
     title: 'Kanban',
     loadChildren: () =>
       import('./kanban/kanban.module').then((m) => m.KanbanModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectUnauthorizedToLogin },
   },
   {
     path: 'auth',
     title: 'Authnetication',
     loadChildren: () => import('./auth/auth.module').then((m) => m.AuthModule),
+    canActivate: [AuthGuard],
+    data: { authGuardPipe: redirectLoggedInToItems },
   },
 ];
 
--- src/app/core/shell/shell.component.ts
+++ src/app/core/shell/shell.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
-import { Auth } from '@angular/fire/auth';
+import { Auth, onAuthStateChanged, User } from '@angular/fire/auth';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-shell',
   templateUrl: './shell.component.html',
   styleUrls: ['./shell.component.scss'],
 })
-export class ShellComponent {
-  constructor(public auth: Auth) {}
+export class ShellComponent implements OnInit {
+  user?: User;
+
+  constructor(public auth: Auth, private router: Router) {}
+
+  ngOnInit(): void {
+    onAuthStateChanged(this.auth, (user) => {
+      if (user) {
+        this.router.navigate(['/']);
+      }
+      this.user = user!;
+    });
+  }
+
+  signOut() {
+    this.auth.signOut().then(() => {
+      this.router.navigate(['auth']);
+    });
+  }
 }
